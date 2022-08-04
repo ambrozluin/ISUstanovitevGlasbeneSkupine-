ab2c08d002953f919bbaac58cf9d9e0d1e7eff0e
Ambrož Luin <ambroz.luin@gmail.com>
2022-08-04 17:01:26 +0200

    IndexPage
    Postavitev in urejanje PrveStrani

---
+++ ISZaGlasbeneSkupine/resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+ 
+    @include('components.hero')
+
+  
+    @include('components.search')
+  
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+  
+
+    
+  
+
+  
+    </div>
+  
+    <div class="mt-6 p-4">
+
+    </div>
+</x-layout>
+  
