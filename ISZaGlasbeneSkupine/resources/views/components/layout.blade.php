ed042821a283a3daf045b2998d1c4a934268b915
Ambrož Luin <ambroz.luin@gmail.com>
2022-08-09 23:30:09 +0200

    CSS

--- ISZaGlasbeneSkupine/resources/views/components/layout.blade.php
+++ ISZaGlasbeneSkupine/resources/views/components/layout.blade.php
@@ -5,10 +5,15 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- za internet explorer !-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link href="{{ asset('css/responsiveFixedAnimatedNavBar.css') }}" rel="stylesheet">
+  <script type="text/javascript" src="{{ asset('js/responsiveFixedAnimatedNavBar.js') }}"></script>
+  
   <link rel="icon" href="images/favicon.ico" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/
   integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <script src="//unpkg.com/alpinejs" defer></script>
   <script src="https://cdn.tailwindcss.com"></script>
   <script>
@@ -22,10 +27,14 @@
         },
       }
   </script>
+
   <title>ISZaGlasbeneSkupine</title>
 </head>
 
-<body class="mb-48">
+
+
+
+<body class="mb-48 ">
   <nav class="flex justify-between items-center mb-4">
     <a href="/"><img class="w-24" src="{{asset('slike/logo.jpg')}}" alt="" class="logo" /></a>
     <ul class="flex space-x-6 mr-6 text-lg">
@@ -58,6 +67,10 @@
       @endauth
     </ul>
   </nav>
+
+  
+
+
 
   <main>
     {{$slot}}
