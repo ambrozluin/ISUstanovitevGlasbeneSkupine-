ad3556a8103e037b90a5a5995cef3c363147337c
Ambrož Luin <ambroz.luin@gmail.com>
2022-08-13 15:20:59 +0200

    UpdateGlasbenaSkupinaController

--- ISZaGlasbeneSkupine/app/Http/Controllers/GlasbenaskupinaController.php
+++ ISZaGlasbeneSkupine/app/Http/Controllers/GlasbenaskupinaController.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Facades\Auth;
 
 class GlasbenaskupinaController extends Controller
 {
-    // Show all listings
+    // Show all Music groups
     public function index() {
         return view('glasbeneskupine.index', [
             'glasbeneskupine' => Glasbenaskupina::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
-    //Show single listing
+    //Show single Music group
     public function show(Glasbenaskupina $glasbenaskupina) {
         $authUser = Auth::id();
         $users = User::all();
@@ -32,7 +32,7 @@ class GlasbenaskupinaController extends Controller
         return view('glasbeneskupine.create');
     }
 
-    // Store Listing Data
+    // Store Music group Data
     public function store(Request $request) {
         $formFields = $request->validate([
             'imeskupine' => 'required',
@@ -58,7 +58,7 @@ class GlasbenaskupinaController extends Controller
         return view('glasbeneskupine.edit', ['glasbenaskupina' => $glasbenaskupina]);
     }
 
-    // Update Listing Data
+    // Update Music group Data
     public function update(Request $request, Glasbenaskupina $glasbenaskupina) {
         // Make sure logged in user is owner
         if($glasbenaskupina->user_id != auth()->id()) {
@@ -79,10 +79,10 @@ class GlasbenaskupinaController extends Controller
 
         $glasbenaskupina->update($formFields);
 
-        return back()->with('message', 'Listing updated successfully!');
+        return back()->with('message', 'Music group updated successfully!');
     }
 
-    // Delete Listing
+    // Delete Music group
     public function destroy(Glasbenaskupina $glasbenaskupina) {
         // Make sure logged in user is owner
         if($glasbenaskupina->user_id != auth()->id()) {
@@ -90,10 +90,10 @@ class GlasbenaskupinaController extends Controller
         }
         
         $glasbenaskupina->delete();
-        return redirect('/')->with('message', 'Listing deleted successfully');
+        return redirect('/')->with('message', 'Music group deleted successfully');
     }
 
-    // Manage Listings
+    // Manage Music group
     public function manage() {
         return view('glasbeneskupine.manage', ['glasbeneskupine' => auth()->user()->glasbeneskupine()->get()]);
     }
