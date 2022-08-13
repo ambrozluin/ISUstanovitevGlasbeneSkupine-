4939fdafd6e851b4b25a88c00bb933d7a7009352
Ambrož Luin <ambroz.luin@gmail.com>
2022-08-13 18:51:52 +0200

    V obdelavi

--- ISZaGlasbeneSkupine/resources/views/invites/show.blade.php
+++ ISZaGlasbeneSkupine/resources/views/invites/show.blade.php
@@ -18,7 +18,7 @@
                 <th scope="col">Prejemnik</th>
                 <th scope="col">Namen</th>
                 <th scope="col">Instrument</th>
-                <th scope="col">Prošnja</th>
+                <th scope="col">Status</th>
                 </thead>
                 <tbody>
                     @unless($invites->isEmpty())
@@ -50,46 +50,92 @@
                 </tbody>
         </table>
     </x-card>
-</x-layout>
 
-<x-card class="p-10 mx-auto mt-24">
-    <header>
-        <h3 class="text-3xl text-center font-bold my-6 uppercase">
-          Potrjena povabila
-        </h3>
-      </header>
-    <table class="w3-table w3-striped w3-bordered">
-        <thead>
-            <th scope="col">#</th>
-            <th scope="col">Pošiljatelj</th>
-            <th scope="col">Prejemnik</th>
-            <th scope="col">Namen</th>
-            <th scope="col">Instrument</th>
-            <th scope="col">Prošnja</th>
-            </thead>
-            <tbody>
-                @unless($invites->isEmpty())
-                    @auth
-                        @foreach($invites as $invite)
-                            @if ($invite->status=="potrjeno" && (Auth::user()->email==$invite->sender_email) || ((Auth::user()->email==$invite->email))) <!-- restrict and show only messegaes sended to me -->                                    <tr>
-                                <tr> 
-                                    <td scope="row">{{$invite->id}} </td>
-                                    <td scope="row">{{$invite->sender_email}}</td>
-                                    <td scope="row">{{$invite->email}} </td>
-                                    <td scope="row">{{$invite->namen}} </td>
-                                    <td scope="row">{{$invite->instrument}} </td>
-                                    <td scope="row">Sprejeto </td>
-                                </tr>
-                            @endif
-                        @endforeach
-                    @endauth
-                @else
-                <tr class="border-gray-300">
-                <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                    <p class="text-center">Ni potrjenih povabil</p>
-                </td>
-                </tr>
-                @endunless
-            </tbody>
-    </table>
-</x-card>
+    <x-card class="p-10 mx-auto mt-24">
+        <header>
+            <h3 class="text-3xl text-center font-bold my-6 uppercase">
+            Povabila v obdelavi
+            </h3>
+        </header>
+        <table class="w3-table w3-striped w3-bordered">
+            <thead>
+                <th scope="col">#</th>
+                <th scope="col">Pošiljatelj</th>
+                <th scope="col">Prejemnik</th>
+                <th scope="col">Namen</th>
+                <th scope="col">Instrument</th>
+                <th scope="col">Status</th>
+                </thead>
+                <tbody>
+                    @unless($invites->isEmpty())
+                        @auth
+                            @foreach($invites as $invite)
+                                @if ($invite->status=="prejeto")  <!-- restrict and show only messegaes sended to me -->  
+                                    @if (Auth::user()->email==$invite->sender_email )
+                                        <tr> 
+                                            <td scope="row">{{$invite->id}} </td>
+                                            <td scope="row">{{$invite->sender_email}}</td>
+                                            <td scope="row">{{$invite->email}} </td>
+                                            <td scope="row">{{$invite->namen}} </td>
+                                            <td scope="row">{{$invite->instrument}} </td>
+                                            <td scope="row">V obdelavi</td>
+                                        </tr>
+                                    @endif
+                                @endif
+                            @endforeach
+                        @endauth
+                    @else
+                    <tr class="border-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <p class="text-center">Ni potrjenih povabil</p>
+                    </td>
+                    </tr>
+                    @endunless
+                </tbody>
+        </table>
+    </x-card>
+    
+    <x-card class="p-10 mx-auto mt-24">
+        <header>
+            <h3 class="text-3xl text-center font-bold my-6 uppercase">
+            Potrjena povabila
+            </h3>
+        </header>
+        <table class="w3-table w3-striped w3-bordered">
+            <thead>
+                <th scope="col">#</th>
+                <th scope="col">Pošiljatelj</th>
+                <th scope="col">Prejemnik</th>
+                <th scope="col">Namen</th>
+                <th scope="col">Instrument</th>
+                <th scope="col">Status</th>
+                </thead>
+                <tbody>
+                    @unless($invites->isEmpty())
+                        @auth
+                            @foreach($invites as $invite)
+                                @if ($invite->status=="potrjeno")  <!-- restrict and show only messegaes sended to me -->  
+                                    @if (Auth::user()->email==$invite->sender_email || (Auth::user()->email==$invite->email))
+                                        <tr> 
+                                            <td scope="row">{{$invite->id}} </td>
+                                            <td scope="row">{{$invite->sender_email}}</td>
+                                            <td scope="row">{{$invite->email}} </td>
+                                            <td scope="row">{{$invite->namen}} </td>
+                                            <td scope="row">{{$invite->instrument}} </td>
+                                            <td scope="row">Sprejeto </td>
+                                        </tr>
+                                    @endif
+                                @endif
+                            @endforeach
+                        @endauth
+                    @else
+                    <tr class="border-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <p class="text-center">Ni potrjenih povabil</p>
+                    </td>
+                    </tr>
+                    @endunless
+                </tbody>
+        </table>
+    </x-card>
+</x-layout>
