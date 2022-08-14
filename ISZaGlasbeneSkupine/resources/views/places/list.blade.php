da1af9204328661ba9c3944b58b1e12f67bdd8d5
Ambrož Luin <ambroz.luin@gmail.com>
2022-08-14 19:49:00 +0200

    PopravkiPlaces

--- ISZaGlasbeneSkupine/resources/views/places/list.blade.php
+++ ISZaGlasbeneSkupine/resources/views/places/list.blade.php
@@ -2,19 +2,16 @@
     <x-heroPlaces></x-heroPlaces>
     
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
-         
-        @unless(count($places) == 0)
-
         @foreach($places as $place)
             <x-place-card :place="$place" />
         @endforeach
-    
-        @else
-            <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4 text-center font-weight-bold">
-                <x-card>Ni objavljenih oglasov</x-card>
-            </div>
-        @endunless
-        
     </div>
+
+    @if (count($places)==0)
+        <div class="lg:grid lg:grid-cols-1 gap-4 space-y-4 md:space-y-0 mx-4 text-center font-weight-bold">
+            <x-card> Ni objavljenih oglasov </x-card>
+        </div>
+    @endif
+  
 
 </x-layout>
